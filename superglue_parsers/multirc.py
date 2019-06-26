8b6272a4e507c84f4faa6d45b666807885cc5784
SenWu <SenWu@users.noreply.github.com>
2019-06-26 15:40:11 -0700

    Add MultiRC tutorial (#12)
    * multirc tutorial
    
    * Add text to MultiRC tutorial
    
    * Add MultiRC Demo

--- superglue_parsers/multirc.py
+++ superglue_parsers/multirc.py
@@ -5,9 +5,9 @@ import sys
 
 import numpy as np
 import torch
-from task_config import SuperGLUE_LABEL_MAPPING
 
 from snorkel.mtl.data import MultitaskDataset
+from task_config import SuperGLUE_LABEL_MAPPING
 
 sys.path.append("..")  # Adds higher directory to python modules path.
 
@@ -17,16 +17,59 @@ logger = logging.getLogger(__name__)
 TASK_NAME = "MultiRC"
 
 
-def parse(jsonl_path, tokenizer, max_data_samples, max_sequence_length):
+def get_rows(jsonl_path, max_data_samples):
     logger.info(f"Loading data from {jsonl_path}.")
     rows = [json.loads(row) for row in open(jsonl_path, encoding="utf-8")]
+
+    new_rows = []
+
+    for row in rows:
+        # each example has a paragraph field -> (text, questions)
+        # text is the paragraph, which requires some preprocessing
+        # questions is a list of questions,
+        # has fields (question, sentences_used, answers)
+        pid = row["idx"]
+
+        para_sent_list = re.sub(
+            "<b>Sent .{1,2}: </b>", "", row["paragraph"]["text"]
+        ).split("<br>")
+
+        para = " ".join(para_sent_list)
+
+        for ques in row["paragraph"]["questions"]:
+            qid = ques["idx"]
+            question = ques["question"]
+            sent_used = ques["sentences_used"]
+
+            for ans in ques["answers"]:
+                new_row = {}
+
+                aid = ans["idx"]
+                answer = ans["text"]
+
+                new_row["pid"] = pid
+                new_row["qid"] = qid
+                new_row["aid"] = aid
+                new_row["paragraph"] = para
+                new_row["question"] = question
+                new_row["answer"] = answer
+                new_row["paragraph_sent_list"] = para_sent_list
+                new_row["sent_used"] = sent_used
+                new_row["label"] = ans["isAnswer"] if "isAnswer" in ans else False
+                new_rows.append(new_row)
+
     for i in range(2):
-        logger.info(f"Sample {i}: {rows[i]}")
+        logger.info(f"Sample {i}: {new_rows[i]}")
 
     # Truncate to max_data_samples
     if max_data_samples:
-        rows = rows[:max_data_samples]
+        new_rows = new_rows[:max_data_samples]
         logger.info(f"Truncating to {max_data_samples} samples.")
+
+    return new_rows
+
+
+def parse_from_rows(rows, tokenizer, max_sequence_length):
 
     # paragraph ids
     pids = []
@@ -52,60 +95,48 @@ def parse(jsonl_path, tokenizer, max_data_samples, max_sequence_length):
     max_len = -1
 
     for row in rows:
-        # each example has a paragraph field -> (text, questions)
-        # text is the paragraph, which requires some preprocessing
-        # questions is a list of questions,
-        # has fields (question, sentences_used, answers)
-        pid = row["idx"]
-        para = re.sub(
-            "<b>Sent .{1,2}: </b>", "", row["paragraph"]["text"].replace("<br>", " ")
+        pid = row["pid"]
+        qid = row["qid"]
+        aid = row["aid"]
+
+        para_token = tokenizer.tokenize(row["paragraph"])[: max_sequence_length - 2]
+        question_token = tokenizer.tokenize(row["question"])[: max_sequence_length - 2]
+        answer_token = tokenizer.tokenize(row["answer"])[: max_sequence_length - 2]
+
+        # Generate tokens
+        tokens = (
+            ["[CLS]"]
+            + para_token
+            + ["[SEP]"]
+            + question_token
+            + answer_token
+            + ["[SEP]"]
         )
-        para_token = tokenizer.tokenize(para)[: max_sequence_length - 2]
+        # No token segments
+        token_segments = [0] * (len(para_token) + 2) + [0] * (
+            len(question_token) + len(answer_token) + 1
+        )
+        token_ids = tokenizer.convert_tokens_to_ids(tokens)
+        token_masks = [1] * len(token_ids)
 
-        for ques in row["paragraph"]["questions"]:
-            qid = ques["idx"]
-            question = ques["question"]
-            question_token = tokenizer.tokenize(question)[: max_sequence_length - 2]
+        if len(tokens) > max_len:
+            max_len = len(tokens)
 
-            for ans in ques["answers"]:
-                aid = ans["idx"]
-                answer = ans["text"]
-                answer_token = tokenizer.tokenize(answer)[: max_sequence_length - 2]
+        # Add to list
+        paras.append(row["paragraph"])
+        questions.append(row["question"])
+        answers.append(row["answer"])
 
-                # Generate tokens
-                tokens = (
-                    ["[CLS]"]
-                    + para_token
-                    + ["[SEP]"]
-                    + question_token
-                    + answer_token
-                    + ["[SEP]"]
-                )
-                # No token segments
-                token_segments = [0] * (len(para_token) + 2) + [0] * (
-                    len(question_token) + len(answer_token) + 1
-                )
-                token_ids = tokenizer.convert_tokens_to_ids(tokens)
-                token_masks = [1] * len(token_ids)
+        label = row["label"]
+        labels.append(SuperGLUE_LABEL_MAPPING[TASK_NAME][label])
 
-                if len(tokens) > max_len:
-                    max_len = len(tokens)
+        pids.append(pid)
+        qids.append(qid)
+        aids.append(aid)
 
-                # Add to list
-                paras.append(para)
-                questions.append(question)
-                answers.append(answer)
-
-                label = ans["isAnswer"] if "isAnswer" in ans else False
-                labels.append(SuperGLUE_LABEL_MAPPING[TASK_NAME][label])
-
-                pids.append(pid)
-                qids.append(qid)
-                aids.append(aid)
-
-                bert_token_ids.append(torch.LongTensor(token_ids))
-                bert_token_masks.append(torch.LongTensor(token_masks))
-                bert_token_segments.append(torch.LongTensor(token_segments))
+        bert_token_ids.append(torch.LongTensor(token_ids))
+        bert_token_masks.append(torch.LongTensor(token_masks))
+        bert_token_segments.append(torch.LongTensor(token_segments))
 
     labels = torch.from_numpy(np.array(labels))
 
@@ -114,15 +145,20 @@ def parse(jsonl_path, tokenizer, max_data_samples, max_sequence_length):
     return MultitaskDataset(
         name="SuperGLUE",
         X_dict={
-            "pids": pids,
-            "qids": qids,
-            "aids": aids,
-            "paras": paras,
-            "questions": questions,
-            "answers": answers,
+            "pid": pids,
+            "qid": qids,
+            "aid": aids,
+            "para": paras,
+            "question": questions,
+            "answer": answers,
             "token_ids": bert_token_ids,
             "token_masks": bert_token_masks,
             "token_segments": bert_token_segments,
         },
         Y_dict={"labels": labels},
     )
+
+
+def parse(jsonl_path, tokenizer, max_data_samples, max_sequence_length):
+    rows = get_rows(jsonl_path, max_data_samples)
+    return parse_from_rows(rows, tokenizer, max_sequence_length)
--- superglue_tasks/multirc.py
+++ superglue_tasks/multirc.py
@@ -1,14 +1,13 @@
-import collections
 import sys
 from functools import partial
 
-from superglue_modules.bert_module import BertLastCLSModule, BertModule
-from task_config import SuperGLUE_LABEL_MAPPING, SuperGLUE_TASK_METRIC_MAPPING
 from torch import nn
 
 from snorkel.model.metrics import metric_score
 from snorkel.mtl.scorer import Scorer
-from snorkel.mtl.task import Task, Operation
+from snorkel.mtl.task import Operation, Task
+from superglue_modules.bert_module import BertLastCLSModule, BertModule
+from task_config import SuperGLUE_LABEL_MAPPING, SuperGLUE_TASK_METRIC_MAPPING
 
 from . import utils
 
@@ -16,37 +15,6 @@ sys.path.append("..")  # Adds higher directory to python modules path.
 
 
 TASK_NAME = "MultiRC"
-
-
-# custom_metric_funcs #################
-
-
-def em(golds, probs, preds, uids):
-    gt_pds = collections.defaultdict(list)
-
-    raise NotImplementedError("uids were removed; how should we do this now?")
-    for gold, pred, uid in zip(golds, preds, uids):
-        qid = "%%".join(uid.split("%%")[:2])
-        gt_pds[qid].append((gold, pred))
-
-    cnt, tot = 0, 0
-    for gt_pd in gt_pds.values():
-        tot += 1
-        gt, pd = list(zip(*gt_pd))
-        if gt == pd:
-            cnt += 1
-
-    return cnt / tot
-
-
-def em_f1(golds, probs, preds):
-    f1 = metric_score(golds, probs, preds, metric="f1")
-    exact = em(golds, probs, preds)
-
-    return (exact + f1["f1"]) / 2
-
-
-#########################################
 
 
 def build_task(bert_model_name, last_hidden_dropout_prob=0.0):
@@ -66,7 +34,7 @@ def build_task(bert_model_name, last_hidden_dropout_prob=0.0):
         else []
     )
 
-    custom_metric_funcs = {"em": em, "em_f1": em_f1}
+    custom_metric_funcs = {}
 
     loss_fn = partial(utils.ce_loss, f"{TASK_NAME}_pred_head")
     output_fn = partial(utils.output, f"{TASK_NAME}_pred_head")
@@ -86,7 +54,11 @@ def build_task(bert_model_name, last_hidden_dropout_prob=0.0):
             Operation(
                 name=f"{TASK_NAME}_bert_module",
                 module_name="bert_module",
-                inputs=[("_input_", "token_ids"), ("_input_", "token_segments"), ("_input_", "token_masks")],
+                inputs=[
+                    ("_input_", "token_ids"),
+                    ("_input_", "token_segments"),
+                    ("_input_", "token_masks"),
+                ],
             ),
             Operation(
                 name=f"{TASK_NAME}_bert_last_CLS",
